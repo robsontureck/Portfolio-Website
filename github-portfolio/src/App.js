cf0550ed4925bf977025e0defccbbfb6e967051d
robsontureck <163215042+robsontureck@users.noreply.github.com>
2024-04-15 23:02:27 +1000

    NavBar and Routes
    Adding routes for different pages and NavBar to the website.

--- github-portfolio/src/App.js
+++ github-portfolio/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import Portfolio from "./pages/Portfolio";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="resume" element={<Resume />} />
+            <Route path="portfolio" element={<Portfolio />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ github-portfolio/src/pages/Layout.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./Layout.css";
+import { Outlet, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from "reactstrap";
+
+const Layout = (args) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+    console.log("Is Open?", !isOpen); // Check if this logs true/false as expected when clicked
+  };
+  return (
+    <>
+      <Navbar expand="lg" {...args}>
+        <NavbarBrand href="/">GitHub Portfolio</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/resume">
+                Resume
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/portfolio">
+                Portfolio
+              </Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
